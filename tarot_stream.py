8459a41a16ee97255def7ef7a30af09a92ce9893
Aspen Cage <aspenncage@gmail.com>
2023-08-25 21:41:28 -0700

    authentication added

--- tarot_stream.py
+++ tarot_stream.py
@@ -5,48 +5,47 @@ from collections import Counter
 from gptarot import *
 from tarot_deck import *
 
-st.set_page_config(page_title="🔮 GPTarot 🔮")
-st.title(title_text)
-st.write(intro_text)
+def tarot_stream():
+    st.write(intro_text)
 
-reading_type = st.selectbox("Which type of Tarot spread shall we do?", Reading,index=0,key="spread")
+    reading_type = st.selectbox("Which type of Tarot spread shall we do?", Reading,index=0,key="spread")
 
-st.write(gen_reading_text(reading_type))
+    st.write(gen_reading_text(reading_type))
 
-lingo = st.selectbox("What tone and vocabulary should I use in the reading?", Lingo,index=0,key="lingo")
+    lingo = st.selectbox("What tone and vocabulary should I use in the reading?", Lingo,index=0,key="lingo")
 
-deck = Deck()
-question = st.text_input(question_ask)
+    deck = Deck()
+    question = st.text_input(question_ask)
 
 
-if st.checkbox("Randomly draw my three cards"):
-    c1,c2,c3 = deck.draw_random_cards(3)
-    # st.write(f"Your three randomly drawn cards are: `{c1}`,`{c2}`, and `{c3}`.")
+    if st.checkbox("Randomly draw my three cards"):
+        c1,c2,c3 = deck.draw_random_cards(3)
+        # st.write(f"Your three randomly drawn cards are: `{c1}`,`{c2}`, and `{c3}`.")
 
-else: 
-    c1 = st.selectbox(c1_ask,deck.base_cards,key="card_select_1") 
-    c2 = st.selectbox(c2_ask,deck.base_cards,key="card_select_2")
-    c3 = st.selectbox(c3_ask,deck.base_cards, key="card_select_3")
+    else: 
+        c1 = st.selectbox(c1_ask,deck.base_cards,key="card_select_1") 
+        c2 = st.selectbox(c2_ask,deck.base_cards,key="card_select_2")
+        c3 = st.selectbox(c3_ask,deck.base_cards, key="card_select_3")
 
-cs = [c1.name,c2.name,c3.name]
-duplicate_check = lambda l: sum(Counter(l).values()) > len(Counter(l))
+    cs = [c1.name,c2.name,c3.name]
+    duplicate_check = lambda l: sum(Counter(l).values()) > len(Counter(l))
 
-with st.spinner("Accessing large language model tarot reader... 📚🔎"):
-    llm = load_llm()
+    with st.spinner("Accessing large language model tarot reader... 📚🔎"):
+        llm = load_llm()
 
-if st.button("Read the cards"):
+    if st.button("Read the cards"):
 
-    if reading_type == Reading.NULL:
-        st.write("You must select which type of spread you would like before I can conduct a reading.")
+        if reading_type == Reading.NULL:
+            st.write("You must select which type of spread you would like before I can conduct a reading.")
 
-    elif duplicate_check(cs):
-        duplicated = [val for val,count in Counter(cs).items() if count>1] # there can only be 1 duplicated card value in a three card draw 
-        dup_str = f"You cannot select duplicate cards. Currently there are multiple copies of {duplicated[0]} chosen. Please adjust your choices and we will continue with the reading."
-        st.write(dup_str)
+        elif duplicate_check(cs):
+            duplicated = [val for val,count in Counter(cs).items() if count>1] # there can only be 1 duplicated card value in a three card draw 
+            dup_str = f"You cannot select duplicate cards. Currently there are multiple copies of {duplicated[0]} chosen. Please adjust your choices and we will continue with the reading."
+            st.write(dup_str)
 
-    else:
-        with st.spinner(wait_msg):
-            reading = read_three_card_spread(llm,question,c1,c2,c3,reading_type, lingo)
-        
-        st.write("My reading for you:")
-        st.write(reading)
+        else:
+            with st.spinner(wait_msg):
+                reading = read_three_card_spread(llm,question,c1,c2,c3,reading_type, lingo)
+            
+            st.write("My reading for you:")
+            st.write(reading)
